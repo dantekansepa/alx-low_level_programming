46aa6990f7ca1e7419fde0b1cf9736852b2c40ce
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2021-08-27 22:00:49 +0200

    A-Z

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,13 +5,12 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-
+{	
+	
 	char c;
-
-	    for (c = 'a'; c <= 'z'; c++)
-	        putchar(c);
-            putchar('\n');
+		for (c = 'a'; c <= 'z'; c++)
+			putchar(c);
+        	putchar('\n');
 
 return (0);
 }
