9c496a30f71b91593e6e22452062dc111446f43e
datekansepa <vsionpy@gmail.com>
2021-09-02 10:55:28 -0700

    Fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,23 +2,25 @@
 #include <stdio.h>
 
 /**
-* more_numbers - prints 10 times the numbers, from 0 to 14.
-*/
+ * more_numbers - prints the range 0-14 ten times.
+ * Return: nothing.
+ */
 void more_numbers(void)
 {
-	int i;
-	char c;
+	int i, j;
 
-	for (i = 0; i < 10; i++)
+	i = j = 0;
+	while (i < 10)
 	{
-		for (c = 0; c <= 14; c++)
+		while (j <= 14)
 		{
-			if (c > 9)
-			  {
-			    _putchar((c / 10) + '0');
-			    _putchar((c % 10) + '0');
-			  }
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			++j;
 		}
 		_putchar('\n');
+		j = 0;
+		i++;
 	}
 }
