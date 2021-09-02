ad337c6e52e029dfed2be2c3ebbc44087d1ea707
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2021-09-02 10:50:20 +0200

    Uppper letters

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,11 +2,10 @@
 #include <stdio.h>
 
 /**
- *_isupper - checks for uppercase characters
- *@c: Variable text
- *Return 1 - when there is an upper character
- *Return 0: Always
- */
+* _isupper - checks for uppercase character
+* @c: Variable text
+* Return: Always 0.
+*/
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
