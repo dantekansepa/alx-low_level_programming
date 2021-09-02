cc9ea16f004e611e5e5573c4ccad0009237c0617
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2021-09-02 11:22:09 +0200

    Numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
+#include "holberton.h"
 /**
- *print_numbers - is a function that prints the numbers from 0-9
+ * print_numbers - prints the digits 1-9
  */
 void print_numbers(void)
 {
-	char c;
+	int i = 0;
 
-	for (c = '0'; c <= '9'; c++)
+	while (i <= 9)
 	{
-		_putchar(c);
+		_putchar(i + '0');
+		++i;
 	}
 	_putchar('\n');
 }
