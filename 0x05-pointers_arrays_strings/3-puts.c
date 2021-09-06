f3f0e29ad359a6201f47c8e5c61fb5f8e1cba35c
datekansepa <vsionpy@gmail.com>
2021-09-06 05:04:14 -0700

    Minor fix

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 
 /**
- *_putchar - prints a sting followed by a new line
+ *_puts - prints a sting followed by a new line
  *@str: value to be evaluated
  *Return: a string to stdout
  */
-void _putchar(char *str)
+void _puts(char *str)
 {
 int l = 0;
 while (*(str + 1) != '\0')
