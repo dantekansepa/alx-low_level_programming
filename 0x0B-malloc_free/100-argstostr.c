ae57c16d000423e23f31ae570ac5a3e42f41fe5f
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2021-09-21 15:06:08 +0200

    Small changes

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -22,7 +22,8 @@ size++;
 size++;
 }
 size++;
-/* allocate memory for total number of chars and
+/**
+ * allocate memory for total number of chars and
  * new line for each word
  */
 strDup = malloc(sizeof(char) * size);
