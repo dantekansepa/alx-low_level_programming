73af9642ea3f909a971fa4844e2cc762b6e46b33
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2021-09-21 15:10:37 +0200

    Big changee

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,43 +1,50 @@
-#include "main.h"
+#include "notrebloh.h"
+#include <stdio.h>
 #include <stdlib.h>
+
 /**
- * *argstostr - concatenates all arguements to the program
- * @ac: arguement count
- * @av: pointer to arguements
- * Return: pointer to new space in memory or null
- **/
+ * *argstostr - convert arguments on command line to strings
+ * @ac: int type
+ * @av: pointer to array
+ * Return: arguments as strings
+ */
+
 char *argstostr(int ac, char **av)
 {
-char *strDup;
-int i, j, k, size;
+int size, count, count1, count2 = 0;
+char *ptr;
 
 if (ac == 0 || av == NULL)
+{
 return (NULL);
-size = 0;
-/* count the number of chars in each string */
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j] != '\0'; j++)
-size++;
-size++;
 }
-size++;
-/**
- * allocate memory for total number of chars and
- * new line for each word
- */
-strDup = malloc(sizeof(char) * size);
-if (strDup == NULL)
+
+for (count = 0; count < ac; count++)
+{
+for (count1 = 0; av[count][count1] != '\0'; count1++)
+{
+size += 1;
+}
+size += 1;
+}
+size += 1;
+
+ptr = malloc(sizeof(char) * size);
+if (ptr == NULL)
+{
+free(ptr);
 return (NULL);
-k = 0;
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j] != '\0'; j++)
-{
-strDup[k++] = av[i][j];
 }
-strDup[k++] = '\n';
+for (count = 0; count < ac; count++)
+{
+for (count1 = 0; av[count][count1] != '\0'; count1++)
+{
+ptr[count2] = av[count][count1];
+count2++;
 }
-strDup[k] = '\0';
-return (strDup);
+ptr[count2] = '\n';
+count2++;
+}
+ptr[count2] = '\0';
+return (ptr);
 }
