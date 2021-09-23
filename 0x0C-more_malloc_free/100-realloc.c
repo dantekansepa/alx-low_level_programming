7416d83bd4b8655c09b3e695af4e693d2b257682
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2021-09-23 19:00:06 +0200

    Back

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,66 +1,51 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- *_realloc-Reallocates a block of memory.
- *@ptr:pointer to the memory previously allocated
- *@old_size:size of the old memory block.
- *@new_size:New size of the block of memory.
- *
- *Return:Pointer
- */
-
+ * *_realloc -  reallocates a memory block using malloc and free
+ * @ptr: void pointer
+ * @old_size: already allocated size
+ * @new_size: new size to allocate
+ * Return: pointer to newly allocated memory or null
+ **/
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-void *newptr;
+	unsigned char *np;
+	unsigned int i;
 
-if (ptr == NULL)
-{
-newptr = malloc(new_size);
-if (newptr == NULL)
-{
-free(ptr);
-return (NULL);
-}
-free(ptr);
-return (newptr);
-}
-if (new_size == old_size)
-return (ptr);
-if (new_size == 0 && ptr != NULL)
-{
-free(ptr);
-return (NULL);
-}
-newptr = malloc(new_size);
-if (newptr == NULL)
-{
-free(ptr);
-return (NULL);
-}
-if (new_size > old_size)
-_memcpy(newptr, ptr, old_size);
-free(ptr);
-return (newptr);
-}
-
-/**
- * _memcpy - copies memory area.
- * @dest: dest pointer
- * @src: source pointer
- * @n: size of bytes
- * Return: pointer to memory area.
- */
-
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-unsigned int i = 0;
-unsigned int j = 0;
-
-while (i < n)
-{
-*(dest + i) = *(src + j);
-i += 1;
-j += 1;
-}
-return (dest);
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size * sizeof(void *));
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
+	np = malloc(new_size * sizeof(char));
+	if (np == NULL)
+		return (NULL);
+	i = 0;
+	if (new_size > old_size)
+	{
+		while (i < old_size)
+		{
+			np[i] = ((char *)ptr)[i];
+			i++;
+		}
+		free(ptr);
+		return (np);
+	}
+/* if new_size < old_size */
+	while (i < new_size)
+	{
+		np[i] = ((char *)ptr)[i];
+		i++;
+	}
+	free(ptr);
+	return (np);
 }
