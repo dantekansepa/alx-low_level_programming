535cb277722c56851efe9e54fe524f4400942d07
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2021-09-23 18:28:42 +0200

    Fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,39 +1,54 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * *string_nconcat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * @n: limit of s2
- * Return: pointer to new space in memory or null
- **/
+ * string_nconcat - Concatenates two strings using at
+ * most an inputted number of bytes.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The maximum number of bytes of s2 to concatenate to s1.
+ *
+ * Return: If the function fails - NULL.
+ * Otherwise - a pointer to the concatenated space in memory.
+ */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *strDup;
-int i;
-unsigned int j;
+char *str;
+unsigned int i, j, m;
 
 if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
 i = 0;
-while (s1[i] != '\0')
-i++;
-strDup = malloc(sizeof(char) * (i + n + 1));
-if (strDup == NULL)
+
+else
+{
+for (i = 0; s1[i]; i++)
+;
+}
+
+if (s2 == NULL)
+j = 0;
+
+else
+{
+for (j = 0; s2[j]; j++)
+;
+}
+
+if (j > n)
+j = n;
+
+str = malloc(sizeof(char) * (i + j + 1));
+
+if (str == NULL)
 return (NULL);
-i = j = 0;
-while (s1[i] != '\0')
-{
-strDup[i] = s1[i];
-i++;
-}
-while (j < n && s2[j] != '\0')
-{
-strDup[i] = s2[j];
-i++, j++;
-}
-strDup[i] = '\0';
-return (strDup);
+
+for (m = 0; m < i; m++)
+str[m] = s1[m];
+
+for (m = 0; m < j; m++)
+str[m + i] = s2[m];
+
+str[i + j] = '\0';
+return (str);
 }
