4e4e7e10d304d3cc2cd9971005cc63c803f10fa5
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2021-09-28 00:06:58 +0200

    Fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,19 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE dog.h
+#ifndef _DOG_H_
+#define _DOG_H_
 /**
- * struct dog - Define a new struct dog with name, age, owner
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of dog
- **/
-struct dog
+ * struct dog - structure containing characteristics of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
