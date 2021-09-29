03f01b36731490f639be09925ec2aaaad0378e0f
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2021-09-29 22:40:11 +0200

    Fix

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,55 +1,74 @@
 #include "3-calc.h"
 /**
- * op_add - adds two ints
- * @a: number 1
- * @b: number 2
- * Return: sum of the two numbers
- **/
+ * op_add - this function adds two numbers
+ * @a:the first number
+ * @b:the second number.
+(* a blank line
+* Description: this function adds two numbers)?
+(* section header: 3-calc.h)*
+* Return: return the result of the add.
+*/
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 /**
- * op_sub - subtract two numbers
- * @a: number 1
- * @b: number 2
- * Return: result of substraction
- **/
+ * op_sub - this function sub two numbers
+ * @a:the first number
+ * @b:the second number.
+(* a blank line
+* Description: this function sub two numbers)?
+(* section header: 3-calc.h)*
+* Return: return the result of the sub.
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 /**
- * op_mul - multiply two numbers
- * @a: number 1
- * @b: number 2
- * Return: result of multiplication
- **/
+ * op_mul - this function multiply two numbers
+ * @a:the first number
+ * @b:the second number.
+(* a blank line
+* Description: this function multiply two numbers)?
+(* section header: 3-calc.h)*
+* Return: return the result of the multiply.
+*/
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 /**
- * op_div - divides two numbers
- * @a: number 1
- * @b: number 2
- * Return: result of divison
- **/
+ * op_div - this function divide two numbers
+ * @a:the first number
+ * @b:the second number.
+(* a blank line
+* Description: this function divide two numbers)?
+(* section header: 3-calc.h)*
+* Return: return the result of the divide.
+ */
 int op_div(int a, int b)
 {
-	if (b == 0)
-		return (-1);
-	return (a / b);
+	if (b != 0)
+		return (a / b);
+	printf("Error\n");
+	exit(100);
 }
 /**
- * op_mod - calculates mod of two ints
- * @a: number 1
- * @b: number 2
- * Return: mod of 1, 2
- **/
+ * op_mod - this function calcualte the module
+ * @a:the first number
+ * @b:the second number.
+(* a blank line
+* Description: this function calculate the module)?
+(* section header: 3-calc.h)*
+* Return: return the result of the module.
+*/
 int op_mod(int a, int b)
 {
 	if (b == 0)
-		return (-1);
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
