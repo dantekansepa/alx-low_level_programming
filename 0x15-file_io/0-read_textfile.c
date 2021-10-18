367d041d6c37e54494873cc051bbb42a6712dd1d
dantekansepa <88306354+dantekansepa@users.noreply.github.com>
2021-10-18 16:03:00 +0200

    Read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,26 +1,43 @@
+#include <stdio.h>
 #include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdlib.h>
 /**
- * *create_array - creates an array of chars, and initializes it with a char
- * @size: size of the array
- * @c: char to be initialized with
- * Return: pointer to array or null
- **/
-char *create_array(unsigned int size, char c)
+ * read_textfile - Reads a text file and prints it to the POSIX
+ * standard output.
+ * @filename: file.
+ * @letters: Number of letters it should read and print.
+ * Return: Actual number of letters it could read and print.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-char *str;
-unsigned int i;
+int fd, res_read, res_write;
+char *buf;
 
-if (size == 0)
-return (NULL);
-str = malloc(sizeof(char) * size);
-if (str == NULL)
-return (NULL);
-i = 0;
-while (i < size)
+if (filename == NULL)
+return (0);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+buf = malloc(sizeof(char) * letters);
+if (buf == NULL)
+return (0);
+res_read = read(fd, buf, letters);
+if (res_read == -1)
 {
-str[i] = c;
-i++;
+free(buf);
+return (0);
 }
-return (str);
+res_write = write(STDOUT_FILENO, buf, res_read);
+if (res_write == -1 || res_read != res_write)
+{
+free(buf);
+return (0);
+}
+free(buf);
+close(fd);
+return (res_write);
 }
